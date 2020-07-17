544526dd3f9762aab71795868dee1f90753a2734
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-07-17 13:47:53 +0200

    axios.all

--- src/components/pages/TopRatedMovies.jsx
+++ src/components/pages/TopRatedMovies.jsx
@@ -11,16 +11,34 @@ class TopRatedMovies extends React.Component {
   }
 
   componentDidMount = () => {
-    this.getTopRatedMovies()
+    this.getAllTopRatedMovies()
   }
 
-  getTopRatedMovies = () => {
-    const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4ec645879aa99a871f6ba0cfa5299287&language=en-US&page=1'
-    axios.get(url)
-      .then(res => res.data)
-      .catch(e => {
-        console.error(e)
-      })
+  // getTopRatedMovies = () => {
+  //   const url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4ec645879aa99a871f6ba0cfa5299287&language=en-US&page=1'
+  //   axios.get(url)
+  //     .then(res => {
+  //       console.log('data', res.data)
+  //     })
+  //     .catch(e => {
+  //       console.error(e)
+  //     })
+  // }
+
+  getAllTopRatedMovies = () => {
+    const url1 = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4ec645879aa99a871f6ba0cfa5299287&language=en-US&page=1'
+    const url2 = 'https://api.themoviedb.org/3/movie/top_rated?api_key=4ec645879aa99a871f6ba0cfa5299287&language=en-US&page=2'
+    axios.all([
+      axios.get(url1),
+      axios.get(url2)
+    ])
+    .then(res => {
+      console.log('data1', res[0].data.results)
+      console.log('data2', res[1].data.results)
+    })
+    .catch(e => {
+      console.error(e)
+    })
   }
 
   render() {
