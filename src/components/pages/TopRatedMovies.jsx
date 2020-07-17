60cabb940c96ee698f032dcfc317fce902886919
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-07-17 17:59:33 +0200

    concat two arrays in the axios.all

--- src/components/pages/TopRatedMovies.jsx
+++ src/components/pages/TopRatedMovies.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import axios from 'axios'
-
+import './TopRatedMovies.css'
 
 class TopRatedMovies extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      movies: []
     }
   }
 
@@ -35,16 +35,31 @@ class TopRatedMovies extends React.Component {
     .then(res => {
       console.log('data1', res[0].data.results)
       console.log('data2', res[1].data.results)
+      const data1 = res[0].data.results
+      const data2 = res[1].data.results
+      const data3 = data1.concat(data2)
+      this.setState({ movies: data3})
     })
     .catch(e => {
       console.error(e)
     })
   }
 
+//   const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
   render() {
+    const urlImage = 'https://image.tmdb.org/t/p/w1280'
     return(
-      <div>
-        <h1>Movies</h1>
+      <div className='listMovies'>
+        {this.state.movies.map(movie =>
+        <Fragment key={movie.id}>
+          <img className='img-movie' src={urlImage + movie.poster_path} alt={movie.id}/>
+          <p>{movie.title}</p>
+          <p>{movie.vote_average}</p>
+        </Fragment>
+        )}
       </div>
     )
   }
